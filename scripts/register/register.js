971fb39de3c8678b0635d53d549b80b2bf98b231
mhusna <mehmethusna@outlook.com>
2025-10-25 00:31:46 +0300

    Kayıt olma işlemi gerçekleştirildi.

---
+++ scripts/register/register.js
@@ -0,0 +1,37 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-auth.js";
+
+// Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyDIGME8_6gN9bI1SCadrsx93QhQRCfC-dM",
+  authDomain: "canli-altin-app.firebaseapp.com",
+  databaseURL:
+    "https://canli-altin-app-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "canli-altin-app",
+  storageBucket: "canli-altin-app.firebasestorage.app",
+  messagingSenderId: "675863034125",
+  appId: "1:675863034125:web:301006180c35a5f0549844",
+  measurementId: "G-V3YHGPSB8M",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const form = document.getElementById("registerForm");
+const message = document.getElementById("message");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            message.textContent = "Kayıt başarılı! Giriş yapabilirsiniz.";
+            form.reset();
+        })
+        .catch((error) => {
+            message.textContent = "Hata: " + error.message;
+        });
+});
