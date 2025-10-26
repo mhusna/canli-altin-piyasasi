a853bceaaae8bdad067137ffb756dab75048ddbe
mhusna <mehmethusna@outlook.com>
2025-10-26 23:29:51 +0300

    update sayfasına yönlendirme yapıldı

---
+++ scripts/livePrices/goToUpdate.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("goToUpdateBtn");
+  if (!btn) return;
+
+  btn.addEventListener("click", () => {
+    window.location.href = "updatePrices.html";
+  });
+});
