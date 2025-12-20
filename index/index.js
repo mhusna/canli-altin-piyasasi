9333a5e3a3e29ae23b2241412a5870896b865a6d
mhusna <mehmethusna@outlook.com>
2025-12-20 18:46:30 +0300

    Kullanıcı girişe gün kontrolü eklendi.

--- index/index.js
+++ index/index.js
@@ -1,6 +1,15 @@
 // app.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-auth.js";
+import { 
+  getFirestore,
+  doc, 
+  getDoc
+} from "https://www.gstatic.com/firebasejs/10.6.0/firebase-firestore.js";
+import { 
+  getAuth, 
+  signInWithEmailAndPassword, 
+  onAuthStateChanged 
+} from "https://www.gstatic.com/firebasejs/10.6.0/firebase-auth.js";
 
 // Firebase ayarları
 const firebaseConfig = {
@@ -17,6 +26,7 @@ const firebaseConfig = {
 // Firebase'i başlat
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
 
 // Form elemanlarını yakala
 const loginForm = document.getElementById("loginForm");
@@ -27,16 +37,36 @@ loginForm.addEventListener("submit", async (e) => {
 
   try {
     await signInWithEmailAndPassword(auth, email, password);
-    console.log("Giriş başarılı!");
-    window.location.href = "../livePrices/livePrices.html"; // yönlendirme
+    debugger
   } catch (error) {
     alert("Giriş hatası: " + error.message);
   }
 });
 
 // Kullanıcı zaten giriş yaptıysa otomatik yönlendir
-onAuthStateChanged(auth, (user) => {
+onAuthStateChanged(auth, async (user) => {
   if (user) {
+    const userRef = doc(db, "users", user.uid);
+    const userSnap = await getDoc(userRef);
+
+    if (!userSnap.exists()) {
+      alert("Kullanıcı bilgileri bulunamadı!");
+      return;
+    }
+
+    const userData = userSnap.data();
+    const expireDate = userData.expireDate?.toDate
+      ? userData.expireDate.toDate()
+      : new Date(userData.expireDate);
+    
+    const currentDate = new Date();
+
+    if (currentDate > expireDate) {
+      alert("Hesabınızın süresi dolmuş. Lütfen yönetici ile iletişime geçin.");
+      await signOut(auth);
+      return;
+    }
+
     window.location.href = "../livePrices/livePrices.html";
   }
 });
